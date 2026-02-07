d0d75ad03d7a5a5b5179c96d0c14fcaebfc9289c
BalwantSandhu <bsingh3@unb.ca>
2026-02-07 01:11:09 -0400

    got routing for transaction page

--- src/app/features/transactions/transactions-routing.module.ts
+++ src/app/features/transactions/transactions-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FundTransferComponent } from './fund-transfer/fund-transfer.component';
+import { TransactionHistoryComponent } from './transaction-history/transaction-history.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'transfer',
+    component: FundTransferComponent
+  },
+  {
+    path: 'history',
+    loadComponent: () =>
+    import('./transaction-history/transaction-history.component')
+    .then(c => c.TransactionHistoryComponent)
+  },
+  {
+    path: '',
+    redirectTo: 'transfer',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
