e3e348e5c9158e8e5f6b0c1e95d590674dc797ef
BalwantSandhu <bsingh3@unb.ca>
2026-02-07 01:08:01 -0400

    implemented the logic for custom button

--- src/app/shared/components/custom-button/custom-button.component.ts
+++ src/app/shared/components/custom-button/custom-button.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AccountType } from '../../../core/models/account.model';
 
 @Component({
   selector: 'app-custom-button',
@@ -7,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './custom-button.component.scss'
 })
 export class CustomButtonComponent {
+  @Input() label: string = 'Submit';
+  @Input() type: 'button' | 'submit' | 'reset' = 'button';
+  @Input() disabled: boolean = false;
+  @Input() accountType?: AccountType;
+  @Input() buttonStyle: 'primary' | 'secondary' | 'danger' = 'primary';
+  @Output() btnClick = new EventEmitter<void>();
 
+  onClick(): void{
+    if(!this.disabled){
+      this.btnClick.emit();
+    }
+  }
+
+  getButtonClass(): string {
+    if(this.accountType){
+      return this.accountType === AccountType.CHEQUING ? 'btn-chequing' : 'btn-savings';
+    }
+    return `btn-${this.buttonStyle}`;
+  }
 }
