5c1441e5c723d78a2d9a4582c1c6acf50b1bdcea
BalwantSandhu <bsingh3@unb.ca>
2026-02-07 17:33:03 -0400

    added structure for account create

--- src/app/features/accounts/create-account/create-account.component.ts
+++ src/app/features/accounts/create-account/create-account.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { RadioOption } from '../../../shared/components/radio-group/radio-group.component';
+import { AccountType } from '../../../core/models/account.model';
+import { Router } from '@angular/router';
+import { AccountService } from '../../../core/services/account.service';
 
 @Component({
   selector: 'app-create-account',
@@ -7,5 +12,67 @@ import { Component } from '@angular/core';
   styleUrl: './create-account.component.scss'
 })
 export class CreateAccountComponent {
+  accountForm!: FormGroup;
+  showSuccess: boolean = false;
+  successMessage: string = '';
 
+  accountTypeOptions: RadioOption[] = [
+    {
+      value: AccountType.CHEQUING,
+      label: 'Chequing Account',
+      description: 'For everyday transactions and payments'
+    },
+    {
+      value: AccountType.SAVINGS,
+      label: 'Savings Account',
+      description: 'For saving money with interest'
+    }
+  ];
+
+  constructor(private router: Router, private accountService: AccountService){}
+
+  ngOnInit(){
+    this.initializeForm();
+  }
+
+  private initializeForm(): void{
+    this.accountForm = new FormGroup({
+      accountHolderName: new FormControl(
+        '',
+        {
+          validators: [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(50)
+          ]
+        }
+      ),
+      accountType: new FormControl(
+        AccountType.CHEQUING,
+        {
+          validators: [
+            Validators.required
+          ]
+        }
+      ),
+      balance: new FormControl(
+        0,
+        {
+          validators: [
+            Validators.required,
+            Validators.min(0),
+            Validators.pattern(/^\d+(\.\d{1,2})?$/)
+          ]
+        }
+      )
+    });
+  }
+
+  get selectedAccountType(): AccountType{
+    return this.accountForm.get('accountType')?.value;
+  }
+
+  goBack(): void{
+    this.router.navigate(['/dashboard']);
+  }
 }
