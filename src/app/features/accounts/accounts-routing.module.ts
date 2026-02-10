788939505f22a6c84be5c27122c521fcb833aba8
BalwantSandhu <bsingh3@unb.ca>
2026-02-10 12:03:36 -0400

    removed unwanted component and fixed a typo

--- src/app/features/accounts/accounts-routing.module.ts
+++ src/app/features/accounts/accounts-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateAccountComponent } from './create-account/create-account.component';
-import { AccountListComponent } from './account-list/account-list.component';
 
 const routes: Routes = [
   {
     path: 'create',
     component: CreateAccountComponent
-  },
-  {
-    path: 'list',
-    component: AccountListComponent
   },
   {
     path: '',
--- src/app/features/accounts/accounts.module.ts
+++ src/app/features/accounts/accounts.module.ts
@@ -4,15 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AccountsRoutingModule } from './accounts-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CreateAccountComponent } from './create-account/create-account.component';
-import { AccountListComponent } from './account-list/account-list.component';
 import { SharedModule } from '../../shared/shared.module';
-import { share } from 'rxjs';
-
 
 @NgModule({
   declarations: [
     CreateAccountComponent,
-    AccountListComponent
   ],
   imports: [
     CommonModule,
